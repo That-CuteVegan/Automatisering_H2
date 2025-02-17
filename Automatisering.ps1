69664d1c47c270fbd676cce91ed69cbf89871d04
That-CuteVegan <magn1.mt@gmail.com>
2025-02-17 12:30:07 +0100

    Update Automatisering.ps1

--- Automatisering.ps1
+++ Automatisering.ps1
@@ -1,3 +1,7 @@
-# init commit
-# Test
-# Git test
+$variable1 = "Hello World"
+$variable2 = 42
+$variable3 = $true
+
+echo $variable1
+echo "The ansewer to everything in the universe is $variable2"
+echo $variable3
