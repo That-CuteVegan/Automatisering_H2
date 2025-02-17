badfb33e49bd3c91929799b930530a04487695bb
That-CuteVegan <magn1.mt@gmail.com>
2025-02-17 13:47:47 +0100

    Update Automatisering.ps1

--- Automatisering.ps1
+++ Automatisering.ps1
@@ -2,6 +2,21 @@ $variable1 = "Hello World"
 $variable2 = 42
 $variable3 = $true
 
-echo $variable1
-echo "The ansewer to everything in the universe is $variable2"
-echo $variable3
+Write-Output $variable1
+Write-Output "The ansewer to everything in the universe is $variable2"
+Write-Output $variable3
+
+# Look in to if active directory is already installed
+$ActiveDirectoryInstall = (Get-WindowsFeature -Name AD-Domain_services).installed
+
+# Checks ActiveDirectoryInstall variable for true or false then either installs it or skips this part
+if ($ActiveDirectoryInstall)
+    {
+        Write-Output "Active Directory is already installed"
+    }
+else
+    {
+        Write-Output "Installing Active Directory"
+        Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+        Write-Output "Active directory have now been installed"
+    }
